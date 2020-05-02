188e05d8987c664ff42feea2dd8ab7fd630df5eb
Fika Handani <fikaahandanii@gmail.com>
2020-05-02 19:47:24 +0700

    Kenalan sekaligus mainan JavaScript menggunakan alert, promt dan confirm

---
+++ javascript.js
@@ -0,0 +1,8 @@
+alert("Tanya siapa orang yang kamu sayang yaaa..");
+var lagi = true;
+while(lagi === true){
+    var nama = prompt("Kamu sayang siapa?")
+    alert("Oh jadi kamu sayang " + nama)
+    lagi = confirm('Tanya lagi?')
+}
+alert("Semoga dia juga sayang kamu")
