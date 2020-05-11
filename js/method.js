5e0669d4fc6678b9c6459f1495947aad0d8082d7
Fika Handani <fikaahandanii@gmail.com>
2020-05-11 23:20:41 +0700

    add slice and splice method

--- js/method.js
+++ js/method.js
@@ -1,5 +1,5 @@
-var array = [1, 2];
-alert("Isi Array Saat ini... Diinget yaa "+array)
+var array = [1, 2, 3, 4, 5];
+//alert("Isi Array Saat ini... Diinget yaa "+array)
 
 function getPush(){
     var input = document.getElementById("push").value
@@ -20,4 +20,41 @@ function getShift(){
     var input = document.getElementById("shift").value
     array.shift(input)
     alert(array)
+}
+function splice(){
+    var position = document.getElementById("posSplice").value
+    var input = document.getElementById("splice").value
+    if(position==""){
+        alert("Posisi Array Kosong")
+    }
+    if(input==""){
+        alert("Nilai Input Splice Kosong")
+    }
+    var length = array.length
+    if(length < position){
+        alert("Posisi Array Melebihi Jumlah Data Pada Array")
+    }
+    else{
+        array.splice(position, 0, input)
+        alert(array)
+    }
+}
+function slice(){
+
+    var indexAwal = document.getElementById("indexAwal").value
+    var indexAkhir = document.getElementById("indexAkhir").value
+    var length = array.length
+    if(indexAwal==""){
+        alert("Index Awal Kosong")
+    }
+    if(indexAkhir==""){
+        alert("Index Akhir Kosong")
+    }
+    if(length < indexAkhir){
+        alert("Index Akhir melebihi Panjang Array")
+    }
+    else{
+        var hasil = array.slice(indexAwal, indexAkhir)
+        alert(hasil)
+    }
 }
